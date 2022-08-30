3a24da85b079abccdeac3a2ff18d058d061a9b3f
Marco Neumann <marco@crepererum.net>
2022-08-30 09:39:28 +0200

    feat: `EventDriver::flush`

--- event_emitter/src/driver.rs
+++ event_emitter/src/driver.rs
@@ -9,7 +9,7 @@ use iox_time::TimeProvider;
 use observability_deps::tracing::log::warn;
 use tokio::{
     runtime::Handle,
-    sync::mpsc::{channel, Sender},
+    sync::mpsc::{channel, error::TrySendError, Sender},
     task::JoinHandle,
 };
 
@@ -18,11 +18,21 @@ use crate::{
     measurement::TypedMeasurement,
 };
 
+/// Internal message from [`EventDriver`]/[`EventRecorder`] to its background worker.
+#[derive(Debug)]
+enum Message {
+    /// Event for emitter.
+    Event(Event<&'static str>),
+
+    /// Flush all batched/queued events and trigger oneshot AFTERWARDS.
+    Flush(tokio::sync::oneshot::Sender<()>),
+}
+
 /// Main driver for event emission.
 #[derive(Debug)]
 pub struct EventDriver {
     _join_handle: JoinHandle<()>,
-    tx: Arc<Sender<Event<&'static str>>>,
+    tx: Arc<Sender<Message>>,
     data_providers: Vec<Box<dyn EventDataProvider>>,
     time_provider: Arc<dyn TimeProvider>,
 }
@@ -37,9 +47,17 @@ impl EventDriver {
     ) -> Self {
         let (tx, mut rx) = channel(1_000);
         let join_handle = handle.spawn(async move {
-            while let Some(event) = rx.recv().await {
-                // TODO: batch events
-                emitter.emit(vec![event]).await;
+            while let Some(msg) = rx.recv().await {
+                match msg {
+                    Message::Event(event) => {
+                        // TODO: batch events
+                        emitter.emit(vec![event]).await;
+                    }
+                    Message::Flush(flush_tx) => {
+                        // We do NOT care if the receiver is gone (e.g. because the flush method was cancelled).
+                        flush_tx.send(()).ok();
+                    }
+                }
             }
         });
 
@@ -68,6 +86,18 @@ impl EventDriver {
             tx: Arc::clone(&self.tx),
         }
     }
+
+    /// Flushes queued events.
+    ///
+    /// Events that are recorded (via [`EventRecorder::drop`]) AFTER calling this function are NOT flushed.
+    pub async fn flush(&self) {
+        let (flush_tx, flush_rx) = tokio::sync::oneshot::channel();
+        self.tx
+            .send(Message::Flush(flush_tx))
+            .await
+            .expect("background worker alive");
+        flush_rx.await.expect("background worker alive");
+    }
 }
 
 /// Event recorder.
@@ -80,7 +110,7 @@ where
 {
     event: Option<Event<M>>,
     do_not_send: bool,
-    tx: Arc<Sender<Event<&'static str>>>,
+    tx: Arc<Sender<Message>>,
 }
 
 impl<M> EventRecorder<M>
@@ -120,8 +150,14 @@ where
     fn drop(&mut self) {
         if !self.do_not_send {
             let event = self.event.take().expect("not yet dropped").untyped();
-            if self.tx.try_send(event).is_err() {
-                warn!("Event buffer full");
+            match self.tx.try_send(Message::Event(event)) {
+                Ok(()) => {}
+                Err(TrySendError::Closed(_)) => {
+                    panic!("Background worker died");
+                }
+                Err(TrySendError::Full(_)) => {
+                    warn!("Event buffer full");
+                }
             }
         }
     }
@@ -129,8 +165,6 @@ where
 
 #[cfg(test)]
 mod tests {
-    use std::time::Duration;
-
     use iox_time::{MockProvider, Time};
 
     use crate::{
@@ -153,8 +187,7 @@ mod tests {
         );
         driver.record::<TestM>().add_tag_mut("my_tag", "xxx");
 
-        // need to sleep a bit to give the background worker a chance to submit the event
-        tokio::time::sleep(Duration::from_millis(10)).await;
+        driver.flush().await;
 
         let expected = Event::new("m", Time::MIN)
             .add_tag_move("foo", "bar")
@@ -173,8 +206,7 @@ mod tests {
         );
         driver.record::<TestM>().do_not_send();
 
-        // need to sleep a bit to give the background worker a chance to submit the event
-        tokio::time::sleep(Duration::from_millis(10)).await;
+        driver.flush().await;
 
         assert_eq!(receiver.read(), vec![]);
     }
